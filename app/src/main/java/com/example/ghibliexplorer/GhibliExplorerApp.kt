78500922913f7bbcf0ee10a4ffae29a80095d685
mccarrascog <maricarmencarrascogutierrez@gmail.com>
2024-10-24 21:22:20 +0200

    Initial setup: Gradle, resources, and structure

---
+++ app/src/main/java/com/example/ghibliexplorer/GhibliExplorerApp.kt
@@ -0,0 +1,4 @@
+package com.example.ghibliexplorer
+
+class GhibliExplorerApp {
+}
---
+++ app/src/main/java/com/example/ghibliexplorer/GhibliExplorerApplication.kt
@@ -0,0 +1,10 @@
+package com.example.ghibliexplorer
+
+import android.app.Application
+
+class GhibliExplorerApplication : Application(){
+    override fun onCreate() {
+        super.onCreate()
+        // Inicializar variables globales o componentes como bases de datos, APIs, etc.
+    }
+}
