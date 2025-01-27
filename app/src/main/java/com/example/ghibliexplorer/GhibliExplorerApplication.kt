38706a727d89f8572decbfa8e79a5b84e6972173
mccarrascog <maricarmencarrascogutierrez@gmail.com>
2025-01-27 19:21:56 +0100

    Add offline app container alongside online container in GhibliExplorerApplication

--- app/src/main/java/com/example/ghibliexplorer/GhibliExplorerApplication.kt
+++ app/src/main/java/com/example/ghibliexplorer/GhibliExplorerApplication.kt
@@ -1,10 +1,16 @@
 package com.example.ghibliexplorer
 
 import android.app.Application
+import com.example.ghibliexplorer.data.offline.DefaultOfflineAppContainer
+import com.example.ghibliexplorer.data.offline.OfflineAppContainer
 import com.example.ghibliexplorer.data.online.DefaultOnlineAppContainer
 import com.example.ghibliexplorer.data.online.OnlineAppContainer
 
 class GhibliExplorerApplication : Application(){
+    val offlineAppContainer: OfflineAppContainer by lazy {
+        DefaultOfflineAppContainer(this)
+    }
+
     lateinit var container: OnlineAppContainer
     override fun onCreate() {
         super.onCreate()
