17f3009c4d1e8be036125cba83fabff917a0d559
mccarrascog <maricarmencarrascogutierrez@gmail.com>
2025-01-27 17:20:10 +0100

    Implement FavFilmsScreen with TopAppBar and integration with FavsViewModel

---
+++ app/src/main/java/com/example/ghibliexplorer/ui/FavFilmsScreen.kt
@@ -0,0 +1,59 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+package com.example.ghibliexplorer.ui
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import com.example.ghibliexplorer.R
+import com.example.ghibliexplorer.ui.screens.viewmodel.FavsViewModel
+import com.example.ghibliexplorer.ui.screens.views.FavsScreen
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FavFilmsScreen(navController: NavController) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = { FavFilmsTopAppBar(scrollBehavior = scrollBehavior) }
+    ) {
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        ) {
+            val favsViewModel: FavsViewModel = viewModel(factory = FavsViewModel.Factory)
+            FavsScreen(
+                favFilmsUiState = favsViewModel.favFilmsUiState,
+                retryAction = { favsViewModel.getFavFilms() },
+                navController = navController
+            )
+        }
+    }
+}
+
+@Composable
+fun FavFilmsTopAppBar(scrollBehavior: TopAppBarScrollBehavior, modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        scrollBehavior = scrollBehavior,
+        title = {
+            Text(
+                text = stringResource(R.string.app_name),
+                style = MaterialTheme.typography.headlineSmall,
+            )
+        },
+        modifier = modifier
+    )
+}
