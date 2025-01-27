7e7225632f860feebee0f0bac8c860fb4e4254e7
mccarrascog <maricarmencarrascogutierrez@gmail.com>
2025-01-27 18:02:45 +0100

    Add FilmDetail screen and Favourites navigation; update GhibliExplorerApp routing

--- app/src/main/java/com/example/ghibliexplorer/GhibliExplorerApp.kt
+++ app/src/main/java/com/example/ghibliexplorer/GhibliExplorerApp.kt
@@ -15,18 +15,30 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.ghibliexplorer.data.online.DefaultOnlineAppContainer
+import com.example.ghibliexplorer.ui.FavFilmsScreen
 import com.example.ghibliexplorer.ui.FilmsScreen
+import com.example.ghibliexplorer.ui.screens.viewmodel.FavsViewModel
+import com.example.ghibliexplorer.ui.screens.views.FilmDetailScreen
 import com.example.ghibliexplorer.ui.screens.views.StartScreen
+import kotlinx.coroutines.runBlocking
 
 enum class GhibliExplorerScreen(@StringRes val titulo: Int){
     Start(titulo = R.string.app_name),
     Home(titulo = R.string.peliculas),
+    FilmDetail(titulo = R.string.detalle_pelicula),
+    Favourites(titulo = R.string.favoritas)
 }
 
 /**
@@ -82,7 +94,7 @@ fun GhibliExplorerApp() {
             composable(route = GhibliExplorerScreen.Start.name) {
                 StartScreen(
                     onStartButtonClicked = {navController.navigate(GhibliExplorerScreen.Home.name)},
-                    onFavouritesButtonClicked = {navController.navigate(GhibliExplorerScreen.Start.name)},
+                    onFavouritesButtonClicked = {navController.navigate(GhibliExplorerScreen.Favourites.name)},
                     modifier = Modifier
                         .fillMaxSize()
                         .padding(innerPadding)
@@ -90,6 +102,48 @@ fun GhibliExplorerApp() {
             }
             composable(route = GhibliExplorerScreen.Home.name) {
                 FilmsScreen(navController = navController, paddingValues = innerPadding)
+            }
+
+            composable(
+                route = "${GhibliExplorerScreen.FilmDetail.name}/{id}",
+                arguments = listOf(navArgument("id"){type = NavType.StringType})
+            ){
+                val filmId = backStackEntry?.arguments?.getString("id")
+                if(filmId!=null){
+                    val filmDetails = runBlocking {
+                        DefaultOnlineAppContainer().OnlineFilmsRepository.getFilmDetails(filmId)
+                    }
+                    filmDetails?.let { film ->
+                        val favsViewModel: FavsViewModel = viewModel(factory = FavsViewModel.Factory)
+                        LaunchedEffect(Unit) {
+                            favsViewModel.checkIsFilmInFavs(film)
+                        }
+
+                        val isFilmInFavsState by favsViewModel.isFilmInFavsState.collectAsState(initial = false)
+
+                        FilmDetailScreen(film = film,
+                            onAddFavouritesButtonClicked = {
+                                favsViewModel.addFilmToFavourites(film)
+                                navController.navigate(GhibliExplorerScreen.Favourites.name)},
+                            onRemoveFromFavsButtonClicked = {
+                                favsViewModel.removeFilmFromFavs(film)
+                                navController.navigate(GhibliExplorerScreen.Favourites.name)
+                            },
+                            isFilmInFavs = isFilmInFavsState,
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(innerPadding)
+                        )
+                    } ?: run {
+                        Text("Film details not found")
+                    }
+                }else{
+                    Text("Invalid film id")
+                }
+            }
+
+            composable(route = GhibliExplorerScreen.Favourites.name) {
+                FavFilmsScreen(navController = navController)
             }
         }
     }
